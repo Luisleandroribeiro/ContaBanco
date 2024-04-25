ecaa6090b4ec76cda3aed2dacf5be54122406abd
Luisleandroribeiro <alemaole.luis@gmail.com>
2024-04-25 14:53:41 -0300

    Projeto ContaBanco Santander DIO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -9,14 +9,17 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o número da conta: ");
         int numero = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.println("Por favor, digite o número da Agência: ");
-        int agencia = scanner.nextInt();
+        String agencia = scanner.nextLine();
 
         System.out.println("Por favor, digite o saldo da Conta: ");
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo
                 +" já está disponível para saque\"");
+        scanner.close();
+
     }
 }
